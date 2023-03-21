927686784b3f52c2968268ae0233febe6b64739d
+ <+>
2023-03-21 17:29:23 -0400

    add scrollup component

---
+++ src/components/ScrollUp.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { BsArrowUpCircleFill } from "react-icons/bs";
+import { Link } from "react-scroll";
+
+const ScrollUp = () => {
+  return (
+    <div>
+      <Link
+        to="navbar"
+        className="fixed bottom-6 right-3 cursor-pointer text-gray-500 hover:text-[#0097b2] md:right-10"
+        spy={true}
+        smooth={true}
+        offset={0}
+        duration={500}
+      >
+        <BsArrowUpCircleFill className="w-8 h-8" />
+      </Link>
+    </div>
+  );
+};
+
+export default ScrollUp;
