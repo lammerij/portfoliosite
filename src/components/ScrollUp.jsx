1c192a9b7fec9246a905b9122785c67b87473366
+ <+>
2023-03-21 17:43:57 -0400

    change scrollup color

--- src/components/ScrollUp.jsx
+++ src/components/ScrollUp.jsx
@@ -7,7 +7,7 @@ const ScrollUp = () => {
     <div>
       <Link
         to="hero"
-        className="fixed bottom-6 right-3 cursor-pointer text-gray-500 hover:text-[#0097b2] md:right-10"
+        className="fixed bottom-6 right-3 cursor-pointer text-gray-500 hover:text-[#f7f7f7] md:right-10"
         spy={true}
         smooth={true}
         offset={0}
