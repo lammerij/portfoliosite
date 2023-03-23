ceb5ad7c4d76743613b6372d5fd0e3d443d1d39e
+ <+>
2023-03-23 13:08:39 -0400

    fix mobile issue

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,10 @@
 import React, { useState } from "react";
-import { FaBars, FaTimes } from "react-icons/fa";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 import { Link } from "react-scroll";
 import logo from "../pics/logo.png";
-import "./Navbar.css";
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
-  const [click, setClick] = useState(false);
-  const handleClick = () => setClick(!click);
-
-  const closeMenu = () => setClick(false);
 
   const handleNav = () => {
     setNav(!nav);
@@ -20,9 +15,18 @@ const Navbar = () => {
       id="navbar"
       className="sticky top-0 z-30 text-white flex justify-between items-center h-24  mx-auto px-4 sm:px-10  bg-black"
     >
-      <Link to="hero" className="cursor-pointer">
-        <img src={logo} className="h-10 w-10 mr-4" alt="logo" />
-      </Link>
+      <div className="flex text-center">
+        <Link
+          to="hero"
+          spy={true}
+          smooth={true}
+          offset={0}
+          duration={500}
+          className="flex items-center cursor-pointer "
+        >
+          <img src={logo} className="h-10 w-10 mr-4" alt="logo" />
+        </Link>
+      </div>
 
       <ul className="hidden md:flex">
         <li className="p-4">
@@ -32,8 +36,8 @@ const Navbar = () => {
             smooth={true}
             offset={-100}
             duration={500}
-            onClick={closeMenu}
-            className="cursor-pointer hover:text-[#fafbfc]"
+            onClick={handleNav}
+            className="cursor-pointer hover:text-[#fc4e4e]"
           >
             Home
           </Link>
@@ -45,8 +49,8 @@ const Navbar = () => {
             smooth={true}
             offset={-100}
             duration={500}
-            onClick={closeMenu}
-            className="cursor-pointer hover:text-[#fafbfc]"
+            onClick={handleNav}
+            className="cursor-pointer hover:text-[#fc4e4e]"
           >
             About
           </Link>
@@ -58,8 +62,8 @@ const Navbar = () => {
             smooth={true}
             offset={-100}
             duration={500}
-            onClick={closeMenu}
-            className="cursor-pointer hover:text-[#fafbfc]"
+            onClick={handleNav}
+            className="cursor-pointer hover:text-[#fc4e4e]"
           >
             Projects
           </Link>
@@ -71,71 +75,77 @@ const Navbar = () => {
             smooth={true}
             offset={-100}
             duration={500}
-            onClick={closeMenu}
-            className="cursor-pointer hover:text-[#fafbfc]"
+            onClick={handleNav}
+            className="cursor-pointer hover:text-[#fc4e4e]"
           >
             Contact
           </Link>
         </li>
       </ul>
-      <div onClick={handleNav} className="block md:hidden">
-        {nav ? <FaTimes size={20} /> : <FaBars size={20} />}
-        <div
-          className={
-            nav
-              ? "fixed z-10 text-white left-0 top-[0px] w-[75%] h-full ease-in-out duration-500  bg-black md:hidden"
-              : "fixed left-[-100%]"
-          }
-        />
+
+      <div onClick={handleNav} className="block m-2 md:hidden">
+        {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+      </div>
+
+      <div
+        className={
+          nav
+            ? "fixed z-10 text-white left-0 top-[0px] w-[75%] h-full ease-in-out duration-500  bg-black md:hidden"
+            : "fixed left-[-100%]"
+        }
+      >
         <ul>
-          <li className="p-4">
+          <li className="p-4 pt-8 border-b  flex">
+            <img src={logo} className="h-10 w-10 mr-4" alt="logo" />
+          </li>
+          <li className="p-4 border-b border-[#fc4e4e]">
             <Link
               to="hero"
               spy={true}
               smooth={true}
               offset={-100}
               duration={500}
-              onClick={closeMenu}
-              className="cursor-pointer hover:text-[#fafbfc]"
+              onClick={handleNav}
+              className="cursor-pointer"
             >
               Home
             </Link>
           </li>
-          <li className="p-4">
+          <li className="p-4 border-b border-[#fc4e4e]">
             <Link
               to="about"
               spy={true}
               smooth={true}
               offset={-100}
               duration={500}
-              onClick={closeMenu}
-              className="cursor-pointer hover:text-[#fafbfc]"
+              onClick={handleNav}
+              className="cursor-pointer"
             >
               About
             </Link>
           </li>
-          <li className="p-4">
+          <li className="p-4 border-b border-[#fc4e4e]">
             <Link
               to="projects"
               spy={true}
               smooth={true}
               offset={-100}
               duration={500}
-              onClick={closeMenu}
-              className="cursor-pointer hover:text-[#fafbfc]"
+              onClick={handleNav}
+              className="cursor-pointer"
             >
               Projects
             </Link>
           </li>
-          <li className="p-4">
+          <li className="p-4 ">
             <Link
               to="contact"
               spy={true}
               smooth={true}
-              offset={50}
+              offset={-100}
               duration={500}
-              onClick={closeMenu}
-              className="cursor-pointer hover:text-[#fafbfc]"
+              onClick={handleNav}
+              className="cursor-pointer"
             >
               Contact
             </Link>
